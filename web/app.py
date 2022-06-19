363f959320e086a89b31f740cd43c1a9d600058a
DaniPalma2002 <72202548+DaniPalma2002@users.noreply.github.com>
2022-06-20 00:55:45 +0100

    update app.py

--- web/app.py
+++ web/app.py
@@ -7,28 +7,47 @@ import psycopg2
 import psycopg2.extras
 
 # SGBD configs
-DB_HOST="127.0.0.1"
-DB_USER="postgres"
-DB_DATABASE=DB_USER
-DB_PASSWORD="1234"
+DB_HOST = "127.0.0.1"
+DB_USER = "postgres"
+DB_DATABASE = DB_USER
+DB_PASSWORD = "pereirawp2002"
 DB_CONNECTION_STRING = "host=%s dbname=%s user=%s password=%s" % (DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD)
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def list_accounts():
-    dbConn=None
-    cursor=None
+    dbConn = None
+    cursor = None
     try:
         dbConn = psycopg2.connect(DB_CONNECTION_STRING)
-        cursor = dbConn.cursor(cursor_factory = psycopg2.extras.DictCursor)
-        query = "SELECT * FROM categoria;"
-        cursor.execute(query)
+        cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        # query = "SELECT * FROM categoria;"
+        # cursor.execute(query)
         return render_template("index.html", cursor=cursor)
     except Exception as e:
-        return str(e) #Renders a page with the error.
+        return str(e)  # Renders a page with the error.
     finally:
         cursor.close()
         dbConn.close()
+
+
+@app.route('/category')
+def list_category_edit():
+    dbConn = None
+    cursor = None
+    try:
+        dbConn = psycopg2.connect(DB_CONNECTION_STRING)
+        cursor = dbConn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        query = "SELECT * FROM categoria;"
+        cursor.execute(query)
+        return render_template("category.html", cursor=cursor)
+    except Exception as e:
+        return str(e)  # Renders a page with the error.
+    finally:
+        cursor.close()
+        dbConn.close()
+
 
 CGIHandler().run(app)
